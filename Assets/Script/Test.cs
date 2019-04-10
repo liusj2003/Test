657689290a3db9c7db89da486395cc0e97209be1
Jason <liusj2003@gmail.com>
2019-04-10 16:38:15 +0100

    Initial check-in of module GithubTest

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    Button btn1;
+    public Text TT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TT.text = "Hello World!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
